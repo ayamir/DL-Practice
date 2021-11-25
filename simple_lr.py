88483092a127032a59900cb995045b78276d7336
ayamir <lgt986452565@gmail.com>
2021-11-25 20:03:29 +0800

    feat: add LR simple implemention.

--- simple_lr.py
+++ simple_lr.py
@@ -1,12 +1,42 @@
 import torch
+import torch.utils.data as Data
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn import init
 import numpy as np
 
-num_inputs = 2
-num_examples = 1000
-true_w = [2, -3.4]
-true_b = 4.2
-features = torch.tensor(np.random.normal(
-    0, 1, (num_examples, num_inputs)), dtype=torch.float)
-labels = true_w[0] * features[:, 0] + true_w * features[:, 1] + true_b
-labels += torch.tensor(np.random.normal(0, 0.01,
-                       size=labels.size()), dtype=torch.float)
+if __name__ == "__main__":
+    num_inputs = 2
+    num_examples = 1000
+    true_w = [2, -3.4]
+    true_b = 4.2
+    features = torch.tensor(np.random.normal(
+        0, 1, (num_examples, num_inputs)), dtype=torch.float)
+    labels = true_w[0] * features[:, 0] + true_w[1] * features[:, 1] + true_b
+    labels += torch.tensor(np.random.normal(0, 0.01,
+                                            size=labels.size()), dtype=torch.float)
+
+    batch_size = 10
+    dataset = Data.TensorDataset(features, labels)
+    data_iter = Data.DataLoader(dataset, batch_size, shuffle=True)
+
+    net = nn.Sequential(nn.Linear(num_inputs, 1))
+    init.normal_(net[0].weight, mean=0, std=0.01)
+    init.constant_(net[0].bias, val=0)
+
+    loss = nn.MSELoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.03)
+
+    num_epochs = 3
+    for epoch in range(1, num_epochs + 1):
+        for X, y in data_iter:
+            output = net(X)
+            l = loss(output, y.view(-1, 1))
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+        print('epoch %d, loss %f' % (epoch, l.item()))
+
+    dense = net[0]
+    print(true_w, dense.weight)
+    print(true_b, dense.bias)
